509f0a96a9b46b3a27258a8976c2b8c60605c6f5
datacamp264 <>
2020-12-29 19:07:02 +0100

    added deployment scripts for kubernetes, added reverseproxy to travis

---
+++ deployment/deplyoment.sh
@@ -0,0 +1,6 @@
+kubectl apply -f udagram-api-auth-deployment.yaml
+kubectl apply -f udagram-api-auth-service.yaml
+kubectl apply -f udagram-api-feed-deployment.yaml
+kubectl apply -f udagram-api-feed-service.yaml
+kubectl apply -f udagram-frontend-deployment.yaml
+kubectl apply -f udagram-frontend-service.yaml
