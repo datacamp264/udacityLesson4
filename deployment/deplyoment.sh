2135dfb56592ce73c526cc5fd089cbfa871ae1bb
datacamp264 <>
2020-12-29 20:29:21 +0100

    changed deployment ports and ngnix logging

--- deployment/deplyoment.sh
+++ deployment/deplyoment.sh
@@ -1,6 +1,11 @@
 kubectl apply -f udagram-api-auth-deployment.yaml
+kubectl autoscale deployment udagram-api-auth --cpu-percent=50 --min=1 --max=2
 kubectl apply -f udagram-api-auth-service.yaml
 kubectl apply -f udagram-api-feed-deployment.yaml
 kubectl apply -f udagram-api-feed-service.yaml
 kubectl apply -f udagram-frontend-deployment.yaml
 kubectl apply -f udagram-frontend-service.yaml
+kubectl apply -f udagram-reverseproxy-deployment.yaml
+kubectl apply -f udagram-reverseproxy-service.yaml
+
+curl --request GET udagram-api-feed:8080/api/v0/feed
